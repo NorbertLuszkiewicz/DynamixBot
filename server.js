29a39162670352c7757179761f3d59bdb2630d39
Glitch (glitch-hello-node) <none>
2021-05-06 11:47:36 +0000

    🛰😻 Checkpoint ./server.js:30693801/144

--- server.js
+++ server.js
@@ -52,8 +52,8 @@ const TWITCHCHANNELS = ["kezman22", "simplywojtek"];
 const OAUTH = process.env.OAUTH;
 const addSongId = "3d0baf73-3272-4ed5-8b06-dc12ad764dc6";
 const skipSongId = "0feec3ff-0f07-4e6c-8113-70e1eb6a8dec";
-const addSongIdWojt = "3d0baf73-3272-4ed5-8b06-dc12ad764dc6";
-const skipSongIdWojt= "0feec3ff-0f07-4e6c-8113-70e1eb6a8dec";
+const addSongIdWojt = "11bcc229-5d3f-4a14-aca7-3b00ace01d7a";
+const skipSongIdWojt= "9150d1d4-51fb-4219-a3ff-92398614029c";
 
 ComfyJS.onChat = (user, message, flags, self, extra) => {
   if (flags.customReward && (extra.customRewardId === addSongId) || (extra.customRewardId === addSongIdWojt) ) {
