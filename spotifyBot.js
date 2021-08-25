dd6a45f7f7a4797ed166326253c0bb0202b11fc0
Glitch (glitch-hello-node) <none>
2021-08-25 05:36:32 +0000

    🍥🔑 Checkpoint ./spotifyBot.js:30693801/2806 ./server.js:30693801/2

--- spotifyBot.js
+++ spotifyBot.js
@@ -1,15 +1,21 @@
 const clientId = process.env.CLIENT_ID;
 const clientSecret = process.env.CLIENT_SECRET;
 const accessToken = "";
+const redirectUri = `https://${process.env.PROJECT_DOMAIN}.glitch.me`;
 const AUTHORIZE = "https://accounts.spotify.com/authorize";
-const redirectUri = process.env.PROJECT_DOMAIN;
+const TOKEN = "https://accounts.spotify.com/api/token";
+const PLAY = "https://api.spotify.com/v1/me/player/play";
+const PAUSE = "https://api.spotify.com/v1/me/player/pause";
+const NEXT = "https://api.spotify.com/v1/me/player/next";
+const CURRENTLYPLAYING =
+  "https://api.spotify.com/v1/me/player/currently-playing";
+let positionMs = 0;
 
 let url = `${AUTHORIZE}?client_id=${clientId}&response_type=code&redirect_uri=${encodeURI(
   redirectUri
 )}&show_dialog=true&scope=user-read-private user-read-email user-modify-playback-state user-read-playback-position user-library-read streaming user-read-playback-state user-read-recently-played playlist-read-private`;
 
-
-const runApi = () => {console.log(url === url2, "urle")};
+const runApi = () => {};
 const startSong = () => {};
 const pauseSong = () => {};
 
