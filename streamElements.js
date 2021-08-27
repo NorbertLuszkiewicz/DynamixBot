51ebf907fe2d110d8fe4f41cd747aa71f170c02f
Glitch (glitch-hello-node) <none>
2021-08-27 19:01:14 +0000

    📫🐴 Checkpoint ./streamElements.js:30693801/3344

--- streamElements.js
+++ streamElements.js
@@ -1,42 +1,22 @@
 var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
-const clientId = process.env.SR_CLIENT_ID;
+const clientId = {process.env.SR_CLIENT_ID;}
 const clientSecret = process.env.SR_CLIENT_SECRET;
+const url = "https://api.streamelements.com/kappa/v2/"
 
-  let body = {};
-
-  callApi(
-    "PUT",
-    PLAY + "?device_id=" + device[streamer],
-    JSON.stringify(body),
-    handleApiResponse
-  );
-
-function callApi(method, url, body, callback) {
-  let xhr = new XMLHttpRequest();
-  xhr.open(method, url, true);
+const currentSong = () => {
+ let xhr = new XMLHttpRequest();
+  xhr.open("Get",`${url}/songrequest/${clientId}/player`, true);
   xhr.setRequestHeader("Content-Type", "application/json");
-  xhr.setRequestHeader("Authorization", "Bearer " + accessToken);
-  xhr.send(body);
-  xhr.onload = callback;
+  xhr.setRequestHeader("Authorization", "Bearer " + clientSecret);
+  xhr.send(null);
+  xhr.onload = handleApiResponse;
 }
 
 function handleApiResponse() {
-  console.log("stop albo start");
   if (this.status == 200) {
-    console.log(this.responseText, "response");
-    action = "";
-    setTimeout(currentlyPlaying, 1000);
-  } else if (this.status == 204) {
-    setTimeout(currentlyPlaying, 1000);
-  } else if (this.status == 401) {
-    console.log("stary token");
-    refreshAccessToken();
+    console.log(this.responseText, "SR currentSong");  
   } else {
-    console.log(this.responseText);
+    console.log(this.responseText, "ERROR SR currentSong ERROR");
   }
-}
-
-const currentSong = () => {
-  
 }
