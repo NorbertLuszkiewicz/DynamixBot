fa11a6dae4c860e77afafd0f1559aae0868efff0
Glitch (glitch-hello-node) <none>
2021-08-27 19:13:15 +0000

    🍝🎿 Checkpoint ./streamElements.js:30693801/1951

--- streamElements.js
+++ streamElements.js
@@ -1,21 +1,27 @@
 var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
-const clientId = {process.env.SR_CLIENT_ID;}
-const clientSecret = process.env.SR_CLIENT_SECRET;
-const url = "https://api.streamelements.com/kappa/v2/"
+const clientId = {
+  kezman22: process.env.SR_CLIENT_ID_KEZMAN,
+  og1ii: process.env.SR_CLIENT_ID_OGI
+};
+const clientSecret = {
+  kezman22: process.env.SR_CLIENT_SECRET_KEZMAN,
+  og1ii: process.env.SR_CLIENT_SECRET_OGI
+};
+const url = "https://api.streamelements.com/kappa/v2/";
 
-const currentSong = () => {
- let xhr = new XMLHttpRequest();
-  xhr.open("Get",`${url}/songrequest/${clientId}/player`, true);
+const currentSong = streamer => {
+  let xhr = new XMLHttpRequest();
+  xhr.open("Get", `${url}/songrequest/${clientId[streamer]}/player`, true);
   xhr.setRequestHeader("Content-Type", "application/json");
-  xhr.setRequestHeader("Authorization", "Bearer " + clientSecret);
+  xhr.setRequestHeader("Authorization", "Bearer " + clientSecret[streamer]);
   xhr.send(null);
   xhr.onload = handleApiResponse;
-}
+};
 
 function handleApiResponse() {
   if (this.status == 200) {
-    console.log(this.responseText, "SR currentSong");  
+    console.log(this.responseText, "SR currentSong");
   } else {
     console.log(this.responseText, "ERROR SR currentSong ERROR");
   }
