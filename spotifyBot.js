07ef4e26f07497becde3f3ddd746bdc68e129874
Glitch (glitch-hello-node) <none>
2021-08-28 05:00:24 +0000

    🍅💆 Checkpoint ./spotifyBot.js:30693801/13766

--- spotifyBot.js
+++ spotifyBot.js
@@ -2,18 +2,6 @@ var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
 const clientId = process.env.CLIENT_ID;
 const clientSecret = process.env.CLIENT_SECRET;
-const clientIdList = [
-  { name: "dynam1x1", id: process.env.CLIENT_ID },
-  { name: "og1ii", id: process.env.CLIENT_ID }
-];
-const clientSecretList = [
-  { name: "dynam1x1", id: process.env.CLIENT_SECRET },
-  { name: "og1ii", id: process.env.CLIENT_SECRET }
-];
-let accessToken =
-  "BQAJ4xsBe8xP9tRyUkDlDC5zmgxK6y2HvUqW_fjXQ3e7RphcC-DIvex6DlA09PamCBmiArNikMiIBcu8jhQifHTJ5zEOlTXpn32L_s0gOZhT_WV-YIwZebkxtn25xV8bl88oo-wMq5GkBRzjqAVfCLtTTKgZqTaILDuMiHThXoUJUMnreFnWetIaFuPh9LeBV5pXo3mQlFc_QyO3wGwNa725Rg";
-const redirectUri = `https://${process.env.PROJECT_DOMAIN}.glitch.me`;
-const AUTHORIZE = "https://accounts.spotify.com/authorize";
 const TOKEN = "https://accounts.spotify.com/api/token";
 const PLAY = "https://api.spotify.com/v1/me/player/play";
 const PAUSE = "https://api.spotify.com/v1/me/player/pause";
@@ -24,30 +12,33 @@ const CURRENTLYPLAYING =
   "https://api.spotify.com/v1/me/player/currently-playing";
 let positionMs = 0;
 let device = {
-  og1ii: "",
+  og1ii: "a00eb394fa4f0ec58111f49101d974acc67b2157",
   kezman22: "c3e9e9038e921489b7106d098ca11128b330ae36",
-  simplywojtek: ""
+  simplywojtek: "",
 };
 
-let url = `${AUTHORIZE}?client_id=${clientId}&response_type=code&redirect_uri=${encodeURI(
-  redirectUri
-)}&show_dialog=true&scope=user-read-private user-read-email user-modify-playback-state user-read-playback-position user-library-read streaming user-read-playback-state user-read-recently-played playlist-read-private`;
-
-let refreshToken =
-  "AQCg9rcXt-DPSVtz9rEqE3fA1x78NqvV6nysu5T9O1EZ1wFWmMJw3yO2budF5OknAiKM5geXUKPXk0Z2RnHm9DMD294V_WdcHJ9Wq4Meg3oRA7YYopjM0sSfpMC1qjQu-w8";
-
-let code = {
-  og1ii: "",
+let refreshTokenList = {
+  og1ii:
+    "AQCYs0az-dh95MDuWB72JAruSc4rj821ERIEK4RpMsEvsQqp5pyzsaqu9kMbqUKsamCI2_gzqyNDkFEEIXE0pHvVX_3_1c3XjfyT-S2NXYKSPl7Ms3w1ZKxsq9ZInJZiezY",
   kezman22:
-    "AQCsHkGOlwiOVXAhKyZcmtcaFgIcRUgL5iGALcRI6foJV4uIHNK_uIdgIMWfKFcrF5dYedaCldmFq8v7I2hpLksID9P_XOgwRHkwnz_ciJ1MXhCOAabYJMOcAurI6Cskxr97sH9k4p6SObCHiom6--5D69VvZv_egq_03tvbpM6UZETOAy1JwTzEcdyx5II032TPY1XywOEoVGTcPY-kOiMofbifXKPoAVRTl7HBZxrSk_kZFJaoEN9I6zfh8e9cDL9vdYCIJkHCKVZG0AQWCEOsH3hnPgKGEq8f6iLfEY-FBLwsX_V9etrNqc9TWGlJION0OBXp5BFzUYLyazKJFUwxkLKB_W39pDiQvruEX4Tyk8Re2avk_NRjRrCQiHf9vQXmbMqQRHKN8WZwF3ZWCxUazcixlZ80rqlGBanpZVVoBffLxxzwYbObsA",
-  simplywojtek: ""
+    "AQCg9rcXt-DPSVtz9rEqE3fA1x78NqvV6nysu5T9O1EZ1wFWmMJw3yO2budF5OknAiKM5geXUKPXk0Z2RnHm9DMD294V_WdcHJ9Wq4Meg3oRA7YYopjM0sSfpMC1qjQu-w8",
+  simplywojtek: "",
 };
-let currentPlaylist = { og1ii: "", kezman22: "", simplywojtek: "" };
-let action = "";
+
+let accessTokenList = {
+  og1ii:
+    "BQBujnjEobElD-6NrVgSYQrHbMzVKy8R8HnYOyZXwZMne-vmupE8wruoSWU58ytwsRtYEZKPpqNQ2ICdlTyC0IzGjJJsYusNyNCDDrmdFutUFn7mSjLIvkNpQyHfhsrx6aT0ysgc7_kDYPPx3A2LOjNsxQDksJG44W-hAb8MnHzkslxBZmmtXfBqZc2UYz1WYCY2txOgB4DVbgbiHphW9a7fAL3fnqRtC-ZrY20Ji_14GqNaEWSB4pmHel3FhD_XvdSY2PNKT3CFcXFDQZJsjQnDbz9Xv-rG",
+  kezman22:
+    "BQAJ4xsBe8xP9tRyUkDlDC5zmgxK6y2HvUqW_fjXQ3e7RphcC-DIvex6DlA09PamCBmiArNikMiIBcu8jhQifHTJ5zEOlTXpn32L_s0gOZhT_WV-YIwZebkxtn25xV8bl88oo-wMq5GkBRzjqAVfCLtTTKgZqTaILDuMiHThXoUJUMnreFnWetIaFuPh9LeBV5pXo3mQlFc_QyO3wGwNa725Rg",
+  simplywojtek: "",
+};
 let maxVolumeDate = null;
 let timeMaxVolume = null;
+let action =""
 
-const startSong = async streamer => {
+let currentPlaylist = { og1ii: "", kezman22: "", simplywojtek: "" };
+
+const startSong = async (streamer) => {
   await refreshAccessToken();
 
   let body = {};
@@ -57,11 +48,25 @@ const startSong = async streamer => {
     "PUT",
     PLAY + "?device_id=" + device[streamer],
     JSON.stringify(body),
-    handleApiResponse
+    handleApiResponse,
+    streamer
   );
 };
 
-const nextSong = streamer => {
+const pauseSong = async (streamer) => {
+  await refreshAccessToken();
+  await callApi(
+    "PUT",
+    PAUSE + "?device_id=" + device[streamer],
+    null,
+    handleApiResponse,
+    streamer
+  );
+
+  console.log("pausesong");
+};
+
+const nextSong = (streamer) => {
   callApi(
     "POST",
     NEXT + "?device_id=" + device[streamer],
@@ -69,7 +74,7 @@ const nextSong = streamer => {
     handleApiResponse
   );
 };
-const changeVolume = streamer => {
+const changeVolume = (streamer) => {
   callApi(
     "PUT",
     `${VOLUME}?volume_percent=${100}&device_id=${device[streamer]}`,
@@ -98,36 +103,17 @@ const changeVolume = streamer => {
   }, maxVolumeDate - now);
 };
 
-const pauseSong = async streamer => {
-  await refreshAccessToken();
-  await callApi(
-    "PUT",
-    PAUSE + "?device_id=" + device[streamer],
-    null,
-    handleApiResponse
-  );
-
-  console.log("pausesong");
-};
-
-function fetchAccessToken() {
-  let body = "grant_type=authorization_code";
-  body += "&code=" + code;
-  body += "&redirect_uri=" + encodeURI(redirectUri);
-  body += "&client_id=" + clientId;
-  body += "&client_secret=" + clientSecret;
-  callAuthorizationApi(body);
-}
-
 function refreshAccessToken() {
-  console.log("refresh token");
-  let body = "grant_type=refresh_token";
-  body += "&refresh_token=" + refreshToken;
-  body += "&client_id=" + clientId;
-  callAuthorizationApi(body);
+  let bodyOgi = `grant_type=refresh_token&refresh_token=${refreshTokenList.og1ii}&client_id=${clientId}`;
+  let bodyKezman = `grant_type=refresh_token&refresh_token=${refreshTokenList.kezman22}&client_id=${clientId}`;
+
+  callAuthorizationApi(bodyOgi, "og1ii");
+  callAuthorizationApi(bodyKezman, "kezman22");
 }
 
-function callAuthorizationApi(body) {
+function callAuthorizationApi(body, streamer) {
+  
+  console.log(body, streamer)
   let xhr = new XMLHttpRequest();
   xhr.open("POST", TOKEN, true);
   xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
@@ -144,29 +130,21 @@ function handleAuthorizationResponse() {
     let data = JSON.parse(this.responseText);
 
     if (data.access_token != undefined) {
-      accessToken = data.access_token;
+      accessTokenList[streamer] = data.access_token;
     }
     if (data.refresh_token != undefined) {
-      refreshToken = data.refresh_token;
-    }
-
-    if (action === "pause") {
-      pauseSong;
-    }
-
-    if (action === "start") {
-      startSong;
+      refreshTokenList[streamer] = data.refresh_token;
     }
   } else {
     console.log(this.responseText);
   }
 }
 
-function callApi(method, url, body, callback) {
+function callApi(method, url, body, callback, streamer) {
   let xhr = new XMLHttpRequest();
   xhr.open(method, url, true);
   xhr.setRequestHeader("Content-Type", "application/json");
-  xhr.setRequestHeader("Authorization", "Bearer " + accessToken);
+  xhr.setRequestHeader("Authorization", "Bearer " + accessTokenList[streamer]);
   xhr.send(body);
   xhr.onload = callback;
 }
@@ -213,9 +191,15 @@ function handleCurrentlyPlayingResponse(streamer) {
   }
 }
 
-function currentlyPlaying() {
+function currentlyPlaying(streamer) {
   console.log("sco gra");
-  callApi("GET", PLAYER + "?market=US", null, handleCurrentlyPlayingResponse);
+  callApi(
+    "GET",
+    PLAYER + "?market=US",
+    null,
+    handleCurrentlyPlayingResponse,
+    streamer
+  );
 }
 
 function refreshDevices() {
@@ -239,11 +223,10 @@ function handleDevicesResponse() {
   }
 }
 
-module.exports = {
-  pauseSong,
+
+module.exports = {   pauseSong,
   startSong,
   nextSong,
   refreshAccessToken,
   refreshDevices,
-  changeVolume
-};
+  changeVolume, };
