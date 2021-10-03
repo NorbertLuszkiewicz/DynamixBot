8691cf1d898604b529a7edf24187169c74003413
Glitch (glitch-hello-node) <none>
2021-10-03 18:46:34 +0000

    🌌👢 Checkpoint ./twitch/messages.js:30693801/20958

--- twitch/messages.js
+++ twitch/messages.js
@@ -73,62 +73,66 @@ const maxVolumeList = [
   }
 ];
 
-const messages = () =>
-  (ComfyJS.onChat = async(user, message, flags, self, extra) => {
-    try{}
-    catch{(err)=> console.log()}
-    
-    getUser
-    
-    addSongIdList.forEach(({ id }) => {
-      if (flags.customReward && extra.customRewardId === id) {
-        ComfyJS.Say("!sr " + message, extra.channel);
-      }
-    });
+const messages = () => {
+  ComfyJS.onChat = async (user, message, flags, self, extra) => {
+    try {
+      const [data] = await getUser(extra.channel);
+      const { addSongID, skipSongID, volumeSongID } = await data;
 
-    if (user == "StreamElements" && message.lastIndexOf("to the queue") != -1) {
-      pauseSong(extra.channel, status => {
-        status == "200" && timeRequest(extra.channel, "add");
-      });
-    }
+        if (flags.customReward && extra.customRewardId === addSongID) {
+          ComfyJS.Say("!sr " + message, extra.channel);
+        }
+      
 
-    skipSongIdList.forEach(({ id }) => {
-      if (flags.customReward && extra.customRewardId === id) {
-        songPlayingNow(extra.channel, function(songPlaying) {
-          if (songPlaying) {
-            ComfyJS.Say("!skip", extra.channel);
-            timeRequest(extra.channel, "skip");
-          } else {
-            nextSong(extra.channel);
-          }
+      if (
+        user == "StreamElements" &&
+        message.lastIndexOf("to the queue") != -1
+      ) {
+        pauseSong(extra.channel, status => {
+          status == "200" && timeRequest(extra.channel, "add");
         });
       }
-    });
 
-    maxVolumeList.forEach(({ id, min, max, minSR, maxSR, time }) => {
-      if (flags.customReward && extra.customRewardId === id) {
-        ComfyJS.Say("!volume " + maxSR, extra.channel);
-        changeVolumeOnTime(extra.channel, min, max, time);
-
-        let now = Date.now();
-
-        if (maxVolumeDate > now) {
-          maxVolumeDate += time;
+      skipSongIdList.forEach(({ id }) => {
+        if (flags.customReward && extra.customRewardId === id) {
+          songPlayingNow(extra.channel, function(songPlaying) {
+            if (songPlaying) {
+              ComfyJS.Say("!skip", extra.channel);
+              timeRequest(extra.channel, "skip");
+            } else {
+              nextSong(extra.channel);
+            }
+          });
         }
+      });
 
-        if (!maxVolumeDate || maxVolumeDate < now) {
-          maxVolumeDate = now + time;
+      maxVolumeList.forEach(({ id, min, max, minSR, maxSR, time }) => {
+        if (flags.customReward && extra.customRewardId === id) {
+          ComfyJS.Say("!volume " + maxSR, extra.channel);
+          changeVolumeOnTime(extra.channel, min, max, time);
+
+          let now = Date.now();
+
+          if (maxVolumeDate > now) {
+            maxVolumeDate += time;
+          }
+
+          if (!maxVolumeDate || maxVolumeDate < now) {
+            maxVolumeDate = now + time;
+          }
+
+          clearTimeout(timeMaxVolume);
+          timeMaxVolume = setTimeout(() => {
+            ComfyJS.Say("!volume " + minSR, extra.channel);
+          }, maxVolumeDate - now);
         }
-
-        clearTimeout(timeMaxVolume);
-        timeMaxVolume = setTimeout(() => {
-          ComfyJS.Say("!volume " + minSR, extra.channel);
-        }, maxVolumeDate - now);
-      }
-    });
+      });
+    } catch {
+      err => console.log(`Error when use song request award ${err}`);
+    }
 
     if (message === "pause" && user === "DynaM1X1") {
-        pauseSong(extra.channel);
+      pauseSong(extra.channel);
     }
 
     if (message === "start" && user === "DynaM1X1") {
@@ -194,13 +198,14 @@ const messages = () =>
 
     if (user == "og1ii" && extra.channel == "l2plelouch") {
       let now = Date.now();
- 
+
       if (timeCooldownOgiii < now) {
         timeCooldownOgiii = 3 * 60 * 1000 + now;
         ComfyJS.Say("^ Dyktator", extra.channel);
       }
     }
-  });
+  };
+};
 
 module.exports = {
   messages
