fcafc8a9a82b38cab39885a578ff1a830627e566
Glitch (glitch-hello-node) <none>
2021-10-04 08:58:32 +0000

    🦁🐞 Checkpoint ./twitch/messages.js:30693801/19390

--- twitch/messages.js
+++ twitch/messages.js
@@ -24,67 +24,68 @@ let timeCooldownOgiii = 0;
 
 const messages = () => {
   ComfyJS.onChat = async (user, message, flags, self, extra) => {
-    try {
-      const [data] = await getUser(extra.channel);
-      const { addSongID, skipSongID, volumeSongID } = await data;
+    const [data] = await getUser(extra.channel);
+    const { addSongID, skipSongID, volumeSongID } = await data;
 
-      if (flags.customReward && extra.customRewardId === addSongID) {
-        ComfyJS.Say("!sr " + message, extra.channel);
+    if (flags.customReward && extra.customRewardId === addSongID) {
+      ComfyJS.Say("!sr " + message, extra.channel);
+    }
+
+    if (
+      user == "StreamElements" &&
+      (message.lastIndexOf("to the queue") != -1 ||
+        message.lastIndexOf("do kolejki"))
+    ) {
+      await pauseSong(extra.channel);
+      await timeRequest(extra.channel, "add");
+    }
+
+    if (flags.customReward && extra.customRewardId === skipSongID) {
+      const { isPlayingNow } = songPlayingNow(extra.channel);
+
+      if (isPlayingNow) {
+        await ComfyJS.Say("!skip", extra.channel);
+        await timeRequest(extra.channel, "skip");
+      } else {
+        nextSong(extra.channel);
+      }
+    }
+
+    const { id, min, max, minSR, maxSR, time } = volumeSongID;
+    if (flags.customReward && extra.customRewardId === id) {
+      ComfyJS.Say("!volume " + maxSR, extra.channel);
+      changeVolumeOnTime(extra.channel, min, max, time);
+
+      let now = Date.now();
+
+      if (maxVolumeDate > now) {
+        maxVolumeDate += time;
       }
 
-      if (
-        user == "StreamElements" &&
-        (message.lastIndexOf("to the queue") != -1 ||
-          message.lastIndexOf("do kolejki"))
-      ) {
-        await pauseSong(extra.channel);
-        await timeRequest(extra.channel, "add");
+      if (!maxVolumeDate || maxVolumeDate < now) {
+        maxVolumeDate = now + time;
       }
 
-      if (flags.customReward && extra.customRewardId === skipSongID) {
-        const { isPlayingNow } = songPlayingNow(extra.channel);
+      clearTimeout(timeMaxVolume);
+      timeMaxVolume = setTimeout(() => {
+        ComfyJS.Say("!volume " + minSR, extra.channel);
+      }, maxVolumeDate - now);
+    }
 
-        if (isPlayingNow) {
-          await ComfyJS.Say("!skip", extra.channel);
-          await timeRequest(extra.channel, "skip");
-        } else {
-          nextSong(extra.channel);
-        }
+    if (message == "skip" && user === "DynaM1X1") {
+      console.log("tu jest");
+      const { isPlayingNow } = songPlayingNow(extra.channel);
+
+      if (isPlayingNow) {
+        ComfyJS.Say("!skip", extra.channel);
+        await timeRequest(extra.channel, "skip");
+      } else {
+        nextSong(extra.channel);
       }
+    }
 
-      const { id, min, max, minSR, maxSR, time } = volumeSongID;
-      if (flags.customReward && extra.customRewardId === id) {
-        ComfyJS.Say("!volume " + maxSR, extra.channel);
-        changeVolumeOnTime(extra.channel, min, max, time);
-
-        let now = Date.now();
-
-        if (maxVolumeDate > now) {
-          maxVolumeDate += time;
-        }
-
-        if (!maxVolumeDate || maxVolumeDate < now) {
-          maxVolumeDate = now + time;
-        }
-
-        clearTimeout(timeMaxVolume);
-        timeMaxVolume = setTimeout(() => {
-          ComfyJS.Say("!volume " + minSR, extra.channel);
-        }, maxVolumeDate - now);
-      }
-
-      if (message === "skip" && user === "DynaM1X1") {
-        const { isPlayingNow } = songPlayingNow(extra.channel);
-
-        if (isPlayingNow) {
-          await ComfyJS.Say("!skip", extra.channel);
-          await timeRequest(extra.channel, "skip");
-        } else {
-          nextSong(extra.channel);
-        }
-      }
-    } catch {
-      err => console.log(`Error when use song request award ${err}`);
+    if (message === "aaa" && user === "DynaM1X1") {
+      ComfyJS.Say("ume ", extra.channel);
     }
 
     if (message === "pause" && user === "DynaM1X1") {
