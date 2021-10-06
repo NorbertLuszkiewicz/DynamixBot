dbe21465dfda7ca687e20f0d7586134e227ef13a
Glitch (glitch-hello-node) <none>
2021-10-06 13:40:55 +0000

    🌖😂 Checkpoint ./server.js:30693801/9

--- server.js
+++ server.js
@@ -106,7 +106,7 @@ fastify.get("/callback", async (req, res) => {
           `http://localhost:3000/dashboard`
         ) :
       res.redirect(
-          `http://localhost:3000/dashboard?error${callback ? callback.status : 400}`
+          `http://localhost:3000/?error${callback ? callback.status : 400}`
         )
 
   } catch {
