4588a4bfa435fbed361352893c0819baa194650e
Glitch (glitch-hello-node) <none>
2021-11-05 09:54:25 +0000

    🐾👵 Checkpoint ./streamElements.js:30693801/67

--- streamElements.js
+++ streamElements.js
@@ -50,7 +50,7 @@ const songPlayingNow = async streamer => {
     const player = await getSpotifyAreaData(streamer, "player");
     const playing = await getSpotifyAreaData(streamer, "playing");
     
-    console.log(player, "player")
+    console.log(player, "player", playing, "aaaa", player.state == "playing" && playing != null)
 
     return {
       isPlayingNow: player.state == "playing" && playing != null,
