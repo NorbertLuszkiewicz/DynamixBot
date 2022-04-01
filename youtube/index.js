8a41b55d81e97ad5d4a255954b91a15eba95a53f
Glitch (glitch-hello-node) <none>
2022-04-01 19:05:32 +0000

    🎟💰 Checkpoint ./youtube/index.js:30693801/1011

--- youtube/index.js
+++ youtube/index.js
@@ -1,10 +1,30 @@
 
+const axios = require("axios");
+
 const isBlockedVideo = async (url, streamer) => {
+    try {
   
   let id = url.slice(url.lastIndexOf("v=") + 2)
   id = id.slice(0, id.indexOf("&"))
-  
-  console.log(url, id)
+
+
+    const  data  = await axios.get(
+      `${url}songrequest/${clientSongRequestID}/${area}`,
+      {
+        headers: {
+          Authorization: `Bearer ${clientSongRequestSecret}`
+        }
+      }
+    );
+      console.log(data)
+
+    return data;
+  } catch (err) {
+    console.log(
+      `Error while getting youtube video (${err} )`
+    );
+  }
+
   
 //   const existThisAccount = data.riotAccountList.find(
 //     (riotAccount) => riotAccount.name == name && riotAccount.server == server
