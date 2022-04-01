b3d898fa8e02b2470c5ceadc42e83953f8d086b5
Glitch (glitch-hello-node) <none>
2022-04-01 19:48:23 +0000

    🏈🏠 Checkpoint ./youtube/index.js:30693801/654

--- youtube/index.js
+++ youtube/index.js
@@ -30,26 +30,6 @@ const isBlockedVideo = async (url, streamer) => {
   } catch (err) {
     console.log(`Error while getting youtube video (${err} )`);
   }
-
-  //   const existThisAccount = data.riotAccountList.find(
-  //     (riotAccount) => riotAccount.name == name && riotAccount.server == server
-  //   );
-
-  //   if (!existThisAccount) {
-  //     const { response } = await api.Summoner.getByName(name, server);
-
-  //     const newRiotAccountList = data.riotAccountList
-  //       ? [
-  //           ...data.riotAccountList,
-  //           { name, server, puuid: response.puuid, id: response.id },
-  //         ]
-  //       : [{ name, server, puuid: response.puuid, id: response.id }];
-
-  //     await updateUser({
-  //       streamer,
-  //       riotAccountList: newRiotAccountList,
-  //     });
-  //   }
 };
 
 module.exports = {
