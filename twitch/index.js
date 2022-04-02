031d16989ba307343acce5ce7c3c1d21fd0b504c
Glitch (glitch-hello-node) <none>
2022-04-02 06:39:40 +0000

    🏇🏔 Checkpoint ./twitch/index.js:30693801/2401

--- twitch/index.js
+++ twitch/index.js
@@ -4,16 +4,32 @@ const { events } = require("./events");
 const { commands } = require("./commands");
 const { getAllUser } = require("../controllers/UserController.js");
 
+let liveStreamers = [];
+
+const setLiveStreamers = () => {
+  try {
+    setTimeout(async () => {
+      const allStreamers = await getAllUser();
+      
+      
+      
+      
+    }, 30000);
+  } catch (err) {
+    console.log(`Error when getting liveStreamers ${err}`);
+  }
+};
+
 const twitchCommands = async () => {
   try {
     messages();
     events();
     commands();
-    setTimeoutVolume()
-    
+    setTimeoutVolume();
+
     const allStreamers = await getAllUser();
 
-    const TWITCHCHANNELS = allStreamers.map(streamer => streamer.streamer);
+    const TWITCHCHANNELS = allStreamers.map((streamer) => streamer.streamer);
 
     const TWITCHUSER = "dynam1x1";
     const OAUTH = process.env.OAUTH;
@@ -25,5 +41,5 @@ const twitchCommands = async () => {
 };
 
 module.exports = {
-  twitchCommands
+  twitchCommands,
 };
