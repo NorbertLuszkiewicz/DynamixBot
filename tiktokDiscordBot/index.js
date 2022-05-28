8b8ed3c7abb019d70ed8be49cb43726943135aed
Glitch (glitch-hello-node) <none>
2022-05-28 20:17:10 +0000

    🍀👢 Checkpoint ./twitch/twitch.js:30693801/6961 ./tiktokDiscordBot/index.js:30693801/9837

--- tiktokDiscordBot/index.js
+++ tiktokDiscordBot/index.js
@@ -1,5 +1,3 @@
-
-
 const runner = () => {
   var __importDefault =
     (this && this.__importDefault) ||
@@ -7,15 +5,18 @@ const runner = () => {
       return mod && mod.__esModule ? mod : { default: mod };
     };
   Object.defineProperty(exports, "__esModule", { value: true });
-const discord = require("discord.js");
+  const discord = require("discord.js");
   const config_1 = require("./config");
-  const client = new discord.Client({ intents: [discord.Intents.FLAGS.GUILDS] });
+  const client = new discord.Client({
+    intents: [discord.Intents.FLAGS.GUILDS],
+  });
 
   client.on("ready", () => {
     console.log(`Logged in as ${client.user.tag}!`);
   });
 
-  client.on("message", (msg) => {
+  client.on("interactionCreate", async (msg) => {
+    console.log(msg);
     if (msg.author.id == client.user.id) return;
     if (msg.guild && config_1.GuildBlacklist.includes(msg.guild.id)) return;
     const matched = config_1.URLRegex.exec(msg.content);
@@ -41,11 +42,44 @@ const discord = require("discord.js");
           ? `\n ${tiktoklinkPlusRest.slice(0, tiktoklinkPlusRest.indexOf(" "))}`
           : "";
 
-      msg.channel.send(
+      await msg.reply(
         `TikTok shared by ${msg.author.tag}\n${config_1.WorkerHost}${url.pathname} ${tiktokUrl}`
       );
     }
   });
+
+//   client.on("message", (msg) => {
+//     console.log(msg);
+//     if (msg.author.id == client.user.id) return;
+//     if (msg.guild && config_1.GuildBlacklist.includes(msg.guild.id)) return;
+//     const matched = config_1.URLRegex.exec(msg.content);
+//     if (matched && matched[0]) {
+//       const url = new URL(matched[0]);
+//       if (!["vm.tiktok.com", "tiktok.com", "www.tiktok.com"].includes(url.host))
+//         return;
+//       if (
+//         ["tiktok.com", "www.tiktok.com"].includes(url.host) &&
+//         !url.pathname.includes("@") &&
+//         !url.pathname.includes("/video/") &&
+//         !url.pathname.includes("/t/")
+//       )
+//         return;
+//       if (msg.content == matched[0] && msg.deletable) msg.delete();
+//       const tiktokPosition =
+//         msg.content.indexOf("vm.tiktok") >= 0
+//           ? msg.content.indexOf("vm.tiktok")
+//           : msg.content.indexOf("tiktok");
+//       const tiktoklinkPlusRest = msg.content.slice(tiktokPosition);
+//       const tiktokUrl =
+//         tiktokPosition >= 0
+//           ? `\n ${tiktoklinkPlusRest.slice(0, tiktoklinkPlusRest.indexOf(" "))}`
+//           : "";
+
+//       msg.channel.send(
+//         `TikTok shared by ${msg.author.tag}\n${config_1.WorkerHost}${url.pathname} ${tiktokUrl}`
+//       );
+//     }
+//   });
 
   client.login(config_1.DiscordToken);
   //# sourceMappingURL=index.js.map
