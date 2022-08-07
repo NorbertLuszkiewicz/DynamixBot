576cee17ddf7906e2e77ad76670b63edac6b520d
Glitch (glitch-hello-node) <none>
2022-08-07 15:22:28 +0000

    🌎👸 Checkpoint ./streamElements.js:30693801/1109

--- streamElements.js
+++ streamElements.js
@@ -194,6 +194,7 @@ const timeRequest = async (streamer, action) => {
 
 const removeBlockedSong = async (streamer) => {
   try {
+    const removedSongList = []
     const [user] = await getUser(streamer);
     const { clientSongRequestID, clientSongRequestSecret } = user;
     const queue = await getSpotifyAreaData(streamer, "queue");
@@ -210,6 +211,7 @@ const removeBlockedSong = async (streamer) => {
         const isBlocked = await isBlockedVideo(null, streamer, song.videoId);
         if (!isBlocked.isVideo || isBlocked.isBlocked) {
           removeSong(song._id);
+          removedSongList.push({user: song.user.username, title: song.title, reason: "ta piosenka jest zablokowana przez yt"})
         }
       });
     }
@@ -218,6 +220,7 @@ const removeBlockedSong = async (streamer) => {
       const isBlocked = await isBlockedVideo(null, streamer, playing.videoId);
       if (!isBlocked.isVideo || isBlocked.isBlocked) {
         removeSong(playing._id);
+        removedSongList.push({user: playing.user.username, title: playing.title, reason: "ta piosenka jest zablokowana przez yt"})
       }
     }
 
@@ -243,14 +246,18 @@ const removeBlockedSong = async (streamer) => {
 
       queue.slice(-3).forEach(async (song) => {
         if (song.source !== "tip") {
+
           if (historyList.find((x) => x === song.videoId)) {
             removeSong(song._id);
+            removedSongList.push({user: song.user.username, title: song.title, reason: "usunięto z kolejki: "})
           }
+          
           if (queue.length > 4) {
             const queueVideoIdList = queue.slice(0, -3).map((x) => x.videoId);
             console.log(queue, queueVideoIdList, queue.slice(-3));
             if (queueVideoIdList.find((x) => x === song.videoId)) {
               removeSong(song._id);
+              removedSongList.push({user: song.user.username, title: song.title, reason: "usunięto z kolejki: ten utwór jest już w kolejce"})
             }
           }
         }
