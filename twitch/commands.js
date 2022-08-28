1ece543e05dae054f37c5e658cea39831037ce0a
Glitch (glitch-hello-node) <none>
2022-08-28 06:01:01 +0000

    🏍💖 Checkpoint ./twitch/commands.js:30693801/23

--- twitch/commands.js
+++ twitch/commands.js
@@ -488,7 +488,7 @@ const commands = () =>
         command === "test" &&
         (flags.mod || flags.broadcaster)
       ) {
-        const respose = await getLolMatchStats("dynam1x1","casual clown","EUW")
+        const respose = await getLolMatchStats("dynam1x1","pilllarmann","EUW")
         ComfyJS.Say(respose, extra.channel);
       }
 
