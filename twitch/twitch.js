c5a6e15524d5d2cdb792f1d20410c776a10e9516
Glitch (glitch-hello-node) <none>
2022-09-13 12:25:45 +0000

    💰🐙 Checkpoint ./twitch/twitch.js:30693801/210

--- twitch/twitch.js
+++ twitch/twitch.js
@@ -136,9 +136,14 @@ const changeBadWords = (message) => {
     .toLowerCase()
     .replace(/nigger/g, "ni**er")
     .replace(/niga/g, "n**a")
-    .replace("nigga/g, "n***a")
-    .replace("czarnuch/g, "cz***uch")
-    .replace(/cwel/g, "c++l");
+    .replace(/nigga/g, "n***a")
+    .replace(/czarnuch/g, "cz***uch")
+    .replace(/cwel/g, "c++l")
+    .replace("nigger", "ni**er")
+    .replace("niga", "n**a")
+    .replace("nigga", "n***a")
+    .replace("czarnuch", "cz***uch")
+    .replace("cwel", "c++l");
 
   return correctMessage == message.toLowerCase() ? message : correctMessage;
 };
