3d838d907c4b535249fc84d2cf86ec7f0871c4a6
Glitch (glitch-hello-node) <none>
2022-11-19 14:37:48 +0000

    🐴🐾 Checkpoint ./twitch/messages.js:30693801/33

--- twitch/messages.js
+++ twitch/messages.js
@@ -349,18 +349,18 @@ const messages = () => {
 
     //usuwa Xd
 
-    if (extra.channel == "kezman22") {
-      const splitMessage = message.split(" ");
+//     if (extra.channel == "kezman22") {
+//       const splitMessage = message.split(" ");
 
-      splitMessage.forEach((x) => {
-        if (
-          (x.indexOf("Xd") !== -1 && x.length > 2) ||
-          x.indexOf("X d") !== -1
-        ) {
-          ComfyJS.Say("/timeout " + user + " 1", extra.channel);
-        }
-      });
-    }
+//       splitMessage.forEach((x) => {
+//         if (
+//           (x.indexOf("Xd") !== -1 && x.length > 2) ||
+//           x.indexOf("X d") !== -1
+//         ) {
+//           ComfyJS.Say("/timeout " + user + " 1", extra.channel);
+//         }
+//       });
+//     }
 
     //cyferki
 
