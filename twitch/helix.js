7cc12c739723eff68d008878591c8c148d37c3b0
Glitch (glitch-hello-node) <none>
2023-03-11 21:31:07 +0000

    🚇🌞 Checkpoint ./twitch/helix.js:30693801/9140

--- twitch/helix.js
+++ twitch/helix.js
@@ -9,22 +9,32 @@ const header = {
 };
 
 const setTwitchHelixToken = async () => {
-  const { data } = await axios.post("https://id.twitch.tv/oauth2/token", {
-    client_id: process.env.BOT_CLIENT_ID,
-    client_secret: process.env.BOT_CLIENT_SECRET,
-    grant_type: "client_credentials",
-  });
+  try {
+    const { data } = await axios.post("https://id.twitch.tv/oauth2/token", {
+      client_id: process.env.BOT_CLIENT_ID,
+      client_secret: process.env.BOT_CLIENT_SECRET,
+      grant_type: "client_credentials",
+    });
 
-  token = data.access_token;
+    token = data.access_token;
 
-  setTimeout(setTwitchHelixToken, data.expires_in - 4000);
+    setTimeout(setTwitchHelixToken, data.expires_in - 4000);
+  } catch (err) {
+    console.log("Error setTwitchHelixToken", err.status);
+  }
 };
 
 const getUserId = async (name) => {
-  console.log(token, "ddasdasd")
-  const { data } = await axios.get(`https://api.twitch.tv/helix/users?login=${name}`, header);
+  try {
+    const { data } = await axios.get(
+      `https://api.twitch.tv/helix/users?login=${name}`,
+      header
+    );
 
-  console.log(data)
+    console.log(data);
+  } catch (err) {
+    console.log("Error getUserId", err.status);
+  }
 };
 
 const timeout = async (userId, duration, reason, streamerId) => {
@@ -33,19 +43,21 @@ const timeout = async (userId, duration, reason, streamerId) => {
     duration,
     reason,
   };
+  try {
+    const { data } = await axios.post(
+      `https://api.twitch.tv/helix/moderation/bans?broadcaster_id=171103106&moderator_id=171103106`,
+      body,
+      header
+    );
 
-  const { data } = await axios.post(
-    `https://api.twitch.tv/helix/moderation/bans?broadcaster_id=171103106&moderator_id=171103106`,
-    body,
-    header
-  );
-
-  console.log(token);
-
+    console.log(data)
+  } catch (err) {
+    console.log("Error timeout function in twitch/helix", err.status);
+  }
 };
 
 module.exports = {
   setTwitchHelixToken,
   timeout,
-  getUserId
+  getUserId,
 };
