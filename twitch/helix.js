a37e0c0eb80952b732c5fb2b68ca001478e52310
Glitch (glitch-hello-node) <none>
2023-03-12 08:06:45 +0000

    🏁👚 Checkpoint ./twitch/helix.js:30693801/98

--- twitch/helix.js
+++ twitch/helix.js
@@ -6,7 +6,7 @@ let token;
 const getHeader = () => {
   return {
     headers: {
-      Authorization: `Bearer ${token}`,
+      Authorization: `Bearer y1m2zo055pce7k0as7g3nd4dd720xd`,
       "Client-Id": process.env.BOT_CLIENT_ID,
       "Content-Type": "application/json",
     },
