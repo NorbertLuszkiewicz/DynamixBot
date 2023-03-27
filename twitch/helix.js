bcae0b84db35596c1debc5e6fbdfdabb484e662b
Glitch (glitch-hello-node) <none>
2023-03-27 14:07:29 +0000

    🍖🌈 Checkpoint ./twitch/helix.js:30693801/38

--- twitch/helix.js
+++ twitch/helix.js
@@ -58,7 +58,7 @@ const timeout = async (userName, duration, reason, streamer) => {
 
   try {
     const { data } = await axios.post(
-      `${URL}moderation/bans?broadcaster_id=${await getUserId(streamer)}&moderator_id=171103106`,
+      `${URL}moderation/bans?broadcaster_id=${await getUserId(streamer)}&moderator_id=process.env.BOT_CLIENT_ID`,
       body,
       await getHeader(streamer)
     );
