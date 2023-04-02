45b428e1fd42a659f079c77c20023d5c90ba8c03
Glitch (glitch-hello-node) <none>
2023-04-02 19:52:07 +0000

    🚆💘 Checkpoint ./routes/index.js:30693801/94

--- routes/index.js
+++ routes/index.js
@@ -149,6 +149,7 @@ async function routes(fastify, options) {
   });
 
   fastify.post("/sendmessage", async (req, res) => {
+    res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "POST");
     console.log(req.body)
     const message = req.body.message;
