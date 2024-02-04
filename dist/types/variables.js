e05cf239fa6f32558e2fbba2dd043e97dece8613
Norbert Luszkiewicz <norbert.luszkiewicz@stxnext.pl>
2024-02-04 08:57:06 +0100

    Remove dist folder

--- dist/types/variables.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.YT_AGE_RESTRICTED = void 0;
-exports.YT_AGE_RESTRICTED = "ytAgeRestricted";
-//# sourceMappingURL=variables.js.map
