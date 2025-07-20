7e88d1730661d7d8a77a4a05c1c226a33926dbc4
Norbert Luszkiewicz <norbertluszkiewicz00@gmail.com>
2025-07-20 08:20:40 +0200

    Update ConfyJS auth

--- src/apis/twitch/index.ts
+++ src/apis/twitch/index.ts
@@ -14,9 +14,9 @@ export const twitchCommands = async (): Promise<void> => {
     const allStreamers = await getAllCredentials();
     const TWITCHCHANNELS = allStreamers.map(streamer => streamer.streamer);
     const TWITCHUSER = "dynam1x1";
-    const OAUTH = process.env.OAUTH;
+    const OAUTH = allStreamers.find(s => s.streamer === TWITCHUSER)?.twitchAccessToken || process.env.OAUTH;
 
-    ComfyJS.Init(TWITCHUSER, OAUTH, TWITCHCHANNELS);
+    ComfyJS.Init(TWITCHUSER, `oauth:${OAUTH}`, TWITCHCHANNELS);
   } catch (err) {
     console.log(`Error while connecting to twitch ${err}`);
   }
